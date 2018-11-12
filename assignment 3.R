1b5e7ca1c09558798577ac17bfdc181f54323c1a
adamjdecaria <41134714+adamjdecaria@users.noreply.github.com>
2018-11-11 22:55:48 -0500

    Add files via upload

--- assignment 3.R
+++ assignment 3.R
@@ -7,11 +7,46 @@ best <- function(state, outcome) {
   try(if (!(outcome %in% outcome_range)) stop("invalid outcome"))
 
   if(outcome == "heart attack") {
-    dat <- data.frame(x = input$State, y = input$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)
+    dat <- data.frame(x = input$State, y = input$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, z = input$Hospital.Name)
     dat <- dat[which(dat$x == state,),]
     cleandat <- data.frame()
     cleandat <- dat[which(dat[, "y"] != "Not Available"),]
-    print(cleandat)
+    cleandat[, "y"] <- as.numeric(as.character(cleandat[, "y"]))
+
+    lowest_mortality_rate <- min(cleandat[, "y"])
+
+    hospital_name <- cleandat[cleandat$y == lowest_mortality_rate, "z"]
+    print(hospital_name)
   }
+
+  else if(outcome == "heart failure") {
+      dat <- data.frame(x = input$State, y = input$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure, z = input$Hospital.Name)
+      dat <- dat[which(dat$x == state,),]
+      cleandat <- data.frame()
+      cleandat <- dat[which(dat[, "y"] != "Not Available"),]
+
+      cleandat[, "y"] <- as.numeric(as.character(cleandat[, "y"]))
+
+      lowest_mortality_rate <- min(cleandat[, "y"])
+
+      hospital_name <- cleandat[cleandat$y == lowest_mortality_rate, "z"]
+      print(hospital_name)
+
+    }
+
+  else if(outcome == "pneumonia") {
+      dat <- data.frame(x = input$State, y = input$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia, z = input$Hospital.Name)
+      dat <- dat[which(dat$x == state,),]
+      cleandat <- data.frame()
+      cleandat <- dat[which(dat[, "y"] != "Not Available"),]
+
+      cleandat[, "y"] <- as.numeric(as.character(cleandat[, "y"]))
+
+      lowest_mortality_rate <- min(cleandat[, "y"])
+
+      hospital_name <- cleandat[cleandat$y == lowest_mortality_rate, "z"]
+      print(hospital_name)
+
+    }
 
 } #end best()
